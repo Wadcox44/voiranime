54d3035cb40916cbe1436ac64a175459e326c804
Wadcox44 <thoneick@hotmail.fr>
2026-04-18 10:09:05 +0200

    Add files via upload

--- anime.js
+++ anime.js
@@ -485,20 +485,8 @@ function renderDetail(anime) {
     const playBtn = el('playTrailerBtn');
 
     if (isMobileOrPi) {
-      // Sur mobile/Pi : ne pas appeler l'API YouTube (quota + restrictions réseau)
-      // Proposer un lien direct vers YouTube à la place
-      if (playBtn) {
-        playBtn.innerHTML = `
-          <svg width="15" height="15" viewBox="0 0 24 24" fill="currentColor">
-            <path d="M23 7s-.3-2-1.2-2.8c-1.1-1.2-2.4-1.2-3-1.3C16.1 2.8 12 2.8 12 2.8s-4.1 0-6.8.2c-.6.1-1.9.1-3 1.3C1.3 5 1 7 1 7S.7 9.2.7 11.3v2c0 2.1.3 4.3.3 4.3s.3 2 1.2 2.8c1.1 1.2 2.6 1.1 3.3 1.2C7.5 21.8 12 22 12 22s4.1 0 6.8-.4c.6-.1 1.9-.1 3-1.3.9-.8 1.2-2.8 1.2-2.8s.3-2.2.3-4.3v-2C23.3 9.2 23 7 23 7zM9.7 15.5V8.4l8.1 3.6-8.1 3.5z"/>
-          </svg>
-          Voir sur YouTube`;
-        playBtn.style.opacity = '1';
-        playBtn.disabled = false;
-        playBtn.onclick = () => {
-          window.open(`https://www.youtube.com/results?search_query=${encodeURIComponent(title + ' trailer anime')}`, '_blank');
-        };
-      }
+      // Sur mobile/Pi sans ID Jikan : masquer le bouton play (le lien est dans les stats)
+      if (playBtn) playBtn.classList.add('hidden');
     } else {
       // Desktop : appel API YouTube normal
       if (playBtn) {
@@ -601,7 +589,7 @@ function renderDetail(anime) {
       <svg width="13" height="13" viewBox="0 0 24 24" fill="currentColor">
         <path d="M23 7s-.3-2-1.2-2.8c-1.1-1.2-2.4-1.2-3-1.3C16.1 2.8 12 2.8 12 2.8s-4.1 0-6.8.2c-.6.1-1.9.1-3 1.3C1.3 5 1 7 1 7S.7 9.2.7 11.3v2c0 2.1.3 4.3.3 4.3s.3 2 1.2 2.8c1.1 1.2 2.6 1.1 3.3 1.2C7.5 21.8 12 22 12 22s4.1 0 6.8-.4c.6-.1 1.9-.1 3-1.3.9-.8 1.2-2.8 1.2-2.8s.3-2.2.3-4.3v-2C23.3 9.2 23 7 23 7zM9.7 15.5V8.4l8.1 3.6-8.1 3.5z"/>
       </svg>
-      Voir bande annonce sur YouTube
+      Voir bande annonce
     </a>` : '';
 
   el('animeQuickStats').innerHTML = stats.map(s => `
